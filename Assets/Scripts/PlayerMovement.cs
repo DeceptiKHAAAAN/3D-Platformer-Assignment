1ad14fc0ee52fa0554ef3e7e83d104a19a73df39
DeceptiKHAAAAN <lukejronquillo@gmail.com>
2026-02-27 19:56:27 -0600

    Implemented Basic Idle and Running Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float gravity = 9.8f;
     [SerializeField] float jumpForce = 6;
 
+    [SerializeField] Animator animator;
+
     CharacterController playerController;
     Vector3 movementVector;
     Camera cam;
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         playerController = GetComponent<CharacterController>();
         cam = Camera.main;
     }
@@ -19,15 +22,17 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
+        // ---------- Relative Camera Movement ----------
         Vector3 camForward  = cam.transform.forward;
         Vector3 camRight    = cam.transform.right;
         camForward.y        = 0;
         camRight.y          = 0;
         camForward          = camForward.normalized;
         camRight            = camRight.normalized;
-
+        
         Vector3 forwardRelativeMovementVector   = v * camForward;
         Vector3 rightRelativeMovmentVector      = h * camRight;
+        // ---------- Relative Camera Movement ----------
 
         Vector3 inputVector = forwardRelativeMovementVector + rightRelativeMovmentVector;
 
@@ -41,6 +46,11 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && playerController.isGrounded)
             movementVector.y = jumpForce;
 
-        playerController.Move(movementVector * Time.deltaTime);
+        if (h != 0 || v != 0)
+            animator.SetBool("isRunning", true);
+        else
+            animator.SetBool("isRunning", false);
+
+            playerController.Move(movementVector * Time.deltaTime);
     }
 }
