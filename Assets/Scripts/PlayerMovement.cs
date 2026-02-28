f46449c8e98cf03c0d509e921697f2d063bf97a6
DeceptiKHAAAAN <lukejronquillo@gmail.com>
2026-02-27 18:38:15 -0600

    Implemented FreeLook Camera and Relative Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,26 +8,37 @@ public class PlayerMovement : MonoBehaviour
 
     CharacterController playerController;
     Vector3 movementVector;
+    Camera cam;
     void Start()
     {
         playerController = GetComponent<CharacterController>();
+        cam = Camera.main;
     }
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        Vector3 inputVector = new Vector3(h, 0, v);
+        Vector3 camForward  = cam.transform.forward;
+        Vector3 camRight    = cam.transform.right;
+        camForward.y        = 0;
+        camRight.y          = 0;
+        camForward          = camForward.normalized;
+        camRight            = camRight.normalized;
+
+        Vector3 forwardRelativeMovementVector   = v * camForward;
+        Vector3 rightRelativeMovmentVector      = h * camRight;
+
+        Vector3 inputVector = forwardRelativeMovementVector + rightRelativeMovmentVector;
 
         movementVector = new Vector3(inputVector.x * moveSpeed  ,
                                      movementVector.y           ,
                                      inputVector.z * moveSpeed  );
 
         movementVector += Vector3.down * gravity * Time.deltaTime;
-
         movementVector.y = Mathf.Clamp(movementVector.y, -14, 14);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && playerController.isGrounded)
             movementVector.y = jumpForce;
 
         playerController.Move(movementVector * Time.deltaTime);
